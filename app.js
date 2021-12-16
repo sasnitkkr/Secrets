eac57262ac9d7e6ebbfffeb7a6a6d8d7425bbf50
Sahil Shende <ssahilshende@gmail.com>
2021-12-16 13:02:09 +0530

    Level 1 - Username and Password only

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 const app = express();
@@ -9,6 +10,17 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+/////////////// Connect to Database ///////////////
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+const userSchema = {
+    email : String,
+    password :  String
+}
+
+const User = mongoose.model('User', userSchema);
+
+/////////////// GET ///////////////
 app.get("/",function(req, res){
     res.render('home.ejs');
 });
@@ -21,6 +33,47 @@ app.get("/login",function(req, res){
     res.render('login.ejs');
 });
 
+/////////////// POST ///////////////
+app.post("/register", function(req, res){
+
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('secrets.ejs');
+        }
+    });
+
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render('secrets.ejs');
+                }else{
+                    console.log("Invalid Password");
+                }
+            }else{
+                console.log("User Not Found");
+            }
+        }
+    });
+
+});
+
+/////////////// Listen ///////////////
 
 app.listen(PORT, function(){
     console.log("App started at port" + PORT);
